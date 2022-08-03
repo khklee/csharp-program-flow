31d541957dc06a4dabe46bf08dc70a5794cb61d0
Kelly Lee <amorfati38@gmail.com>
2022-08-02 20:15:33 -0600

    practice

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using WiredBrainCoffeeSurveys.Reports.Services;
+
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -6,7 +13,41 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            bool quitApp = false;
+
+            do 
+            {
+                Console.WriteLine("Please specify a report to run (rewards, comments, tasks, quit):");
+                var selectedReport = Console.ReadLine();
+
+                Console.WriteLine("Please specify which quarter of data: (q1, q2)");
+                var selectedData = Console.ReadLine();
+
+                var SurveyResults = JsonConvert.DeserializeObject<SurveyResults>
+                    (File.ReadAllText($"data/{selectedData}.json"));
+
+                switch (selectedReport)
+                {
+                    case "rewards":
+                        RewardsReportService.GenerateWinnerEmails(SurveyResults);
+                        break;
+                    case "comments":
+                        CommentsReportService.GenerateCommentsReport(SurveyResults);
+                        break;
+                    case "tasks": 
+                        TasksReportService.GenerateTasksReport(SurveyResults);
+                        break;
+                    case "quit":
+                        quitApp = true;
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, that's not a valid option.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+            while (!quitApp);
         }
     }
 }
